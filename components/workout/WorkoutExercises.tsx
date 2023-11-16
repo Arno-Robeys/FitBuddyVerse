b023d47a96504a8a11460b4b7634f8370c994411
Michiel-Verhulst <90454111+Michiel-Verhulst@users.noreply.github.com>
2023-11-16 18:55:05 +0100

    Workout (excercises)

---
+++ components/workout/WorkoutExercises.tsx
@@ -0,0 +1,29 @@
+import WorkoutExercise from '../../../Mobile Applications - PROJECT OLD/components/workout/WorkoutExercise';
+import { useWorkoutContext } from '../../../Mobile Applications - PROJECT OLD/components/workout/context/WorkoutContext';
+
+export default function WorkoutExercises() {
+    const { workout } = useWorkoutContext();
+
+    const exercises = new Map();
+    workout.exerciseSets?.forEach((exerciseSet) => {
+        if (exercises.has(exerciseSet.exerciseId)) {
+            exercises.set(exerciseSet.exerciseId, exercises.get(exerciseSet.exerciseId) + 1);
+        } else {
+            exercises.set(exerciseSet.exerciseId, 1);
+        }
+    });
+
+    return (
+        <>
+            {Array.from(exercises.keys())?.map((exerciseKey) => {
+                return (
+                    <WorkoutExercise
+                        key={workout.id + exerciseKey}
+                        exerciseId={parseInt(exerciseKey)}
+                        setsAmount={exercises.get(exerciseKey)}
+                    />
+                );
+            })}
+        </>
+    );
+}
