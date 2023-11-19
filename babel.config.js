146483a004212404a81519799c8b025193f95fb1
Arno Robeys <arno.robeys@student.ucll.be>
2023-11-19 17:33:41 +0100

    Rollback Fixes Warnings

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,6 @@ module.exports = function (api) {
 	api.cache(true);
 	return {
 		presets: ["babel-preset-expo"],
-		plugins: ["expo-router/babel", "nativewind/babel",
-		"@babel/plugin-transform-optional-catch-binding",
-		"@babel/plugin-transform-numeric-separator",
-		"@babel/plugin-transform-class-properties",
-		"@babel/plugin-transform-nullish-coalescing-operator",
-		"@babel/plugin-transform-export-namespace-from",
-		"@babel/plugin-transform-optional-chaining",
-		"@babel/plugin-transform-object-rest-spread",
-		"@babel/plugin-transform-async-generator-functions"],
+		plugins: ["expo-router/babel", "nativewind/babel"],
 	};
 };
