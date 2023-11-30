d2357b485ec0c38ee9c3d98f0d99140f0e86e964
Michiel-Verhulst <90454111+Michiel-Verhulst@users.noreply.github.com>
2023-11-30 16:24:15 +0100

    SingUp CSS toegevoegd

--- app/signUp/index.tsx
+++ app/signUp/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import {View, Text, TextInput, Button, TouchableOpacity} from 'react-native';
+import Errors from "@/components/Errors";
+import { router } from "expo-router";
 
 export default function RegistrationForm() {
     const [firstName, setFirstName] = useState('');
@@ -7,7 +9,7 @@ export default function RegistrationForm() {
     const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+    const [errors, setErrors] = useState([]);
 
     const handleRegistration = () => {
         // Implement your registration logic here
@@ -18,40 +20,74 @@ export default function RegistrationForm() {
             username,
             password,
         });
+
+        router.push("/");
         // You can send the registration details to your backend or perform any other actions
     };
 
     return (
-        <View>
-            <Text>Registration Form</Text>
-            <TextInput
-                placeholder="First Name"
-                value={firstName}
-                onChangeText={(text) => setFirstName(text)}
-            />
-            <TextInput
-                placeholder="Last Name"
-                value={lastName}
-                onChangeText={(text) => setLastName(text)}
-            />
-            <TextInput
-                placeholder="Email"
-                value={email}
-                onChangeText={(text) => setEmail(text)}
-                keyboardType="email-address"
-            />
-            <TextInput
-                placeholder="Username"
-                value={username}
-                onChangeText={(text) => setUsername(text)}
-            />
-            <TextInput
-                placeholder="Password"
-                value={password}
-                onChangeText={(text) => setPassword(text)}
-                secureTextEntry
-            />
-            <Button title="Register" onPress={handleRegistration} />
+        <View className="flex-1 justify-center p-4">
+            <Text className="text-center text-2xl font-bold mb-4">
+                Registration Form
+            </Text>
+
+            {errors.length > 0 && (
+                <Errors errors={errors} clear={() => setErrors([])} />
+            )}
+            <View className="mb-4">
+                <Text className="text-base">
+                    First Name
+                </Text>
+                <TextInput className="border border-gray-500 rounded p-2"
+                    placeholder="First Name"
+                    value={firstName}
+                    onChangeText={(text) => setFirstName(text)}
+                />
+
+                <Text className="text-base">
+                    Last Name
+                </Text>
+                <TextInput className="border border-gray-500 rounded p-2"
+                    placeholder="Last Name"
+                    value={lastName}
+                    onChangeText={(text) => setLastName(text)}
+                />
+
+                <Text className="text-base">
+                    Email
+                </Text>
+                <TextInput className="border border-gray-500 rounded p-2"
+                    placeholder="Email"
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                    keyboardType="email-address"
+                />
+
+                <Text className="text-base">
+                    Username
+                </Text>
+                <TextInput className="border border-gray-500 rounded p-2"
+                    placeholder="Username"
+                    value={username}
+                    onChangeText={(text) => setUsername(text)}
+                />
+
+                <Text className="text-base">
+                    Password
+                </Text>
+                <TextInput className="border border-gray-500 rounded p-2"
+                    placeholder="Password"
+                    value={password}
+                    onChangeText={(text) => setPassword(text)}
+                    secureTextEntry
+                />
+            </View>
+
+            <TouchableOpacity onPress={handleRegistration}>
+                <View className="bg-gray-800 rounded p-2 items-center">
+                    <Text className="text-white text-lg">Register</Text>
+                </View>
+            </TouchableOpacity>
         </View>
     );
 }
