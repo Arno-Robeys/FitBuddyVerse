f1ee6d4b0cb0fde9cc2ce0ecb89d752490bda3c9
Arno <arno.robeys@student.ucll.be>
2024-01-03 23:05:10 +0100

    UI Improvement Search

--- frontend/app/search/index.tsx
+++ frontend/app/search/index.tsx
@@ -1,7 +1,7 @@
 import profileService from "@/lib/profileService";
 import { TProfile } from "@/types/profile.type";
 import { Ionicons } from "@expo/vector-icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FlatList, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function SearchPage({ navigation }: { navigation: any }) {
@@ -16,21 +16,29 @@ export default function SearchPage({ navigation }: { navigation: any }) {
 		});
 	}
 
+	useEffect(() => {
+		//Add search bar to navigation
+		navigation.setOptions({
+			headerTitle: () => (
+				<TextInput className="h-12 text-lg" onChangeText={(text) => handleSearch(text)} placeholder="Search for user..." />
+			)
+		});
+	}, []);
+
 	return (
 		<View className="bg-white flex-1 p-4">
-			<View className="border border-gray-500 rounded items-center space-x-2 flex-row mb-2">
-				<Ionicons name="ios-search" size={30} color={'black'} />
-				<TextInput className="h-12 text-lg" onChangeText={(text) => handleSearch(text)} placeholder="Search for user..." />
-			</View>
 			<FlatList
 				data={users.filter(user => user.username.toLowerCase().includes(searchText.toLowerCase()))}
 				keyExtractor={(item) => (item.id as number).toString()}
+				ListHeaderComponent={() => (
+					<Text className="text-base text-gray-400 text-center">{users.length == 0 ? 'No users found' : 'Search Results'}</Text>
+				)}
 				renderItem={({ item }) => (
-					<View>
-						<TouchableOpacity onPress={() => { navigation.navigate("ProfileUser", { id: item.id }) }}>
-							<Text style={{ fontWeight: 'bold', textAlign: 'center', fontSize: 30 }}>{item.username}</Text>
+					<View className="flex-row justify-between border-b-2 py-4 border-gray-300">
+						<TouchableOpacity className="w-3/4" onPress={() => { navigation.navigate("ProfileUser", { id: item.id }) }}>
+							<Text className="text-2xl font-bold">{item.username}</Text>
 						</TouchableOpacity>
-						<TouchableOpacity onPress={() => { }} className="bg-blue-400 rounded mb-5 py-2">
+						<TouchableOpacity onPress={() => { }} className="bg-blue-400 rounded p-2">
 							<Text className="text-center text-white font-bold">FOLLOW</Text>
 						</TouchableOpacity>
 					</View>
