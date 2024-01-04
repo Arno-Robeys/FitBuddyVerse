7db845d9f4af50d2c6c5d2e0e2acde8e0842cc38
Michiel-Verhulst <michielverhulst@telenet.be>
2024-01-04 16:27:45 +0100

    added %ProgramData% in gitignore, profileName showing on workouts

--- frontend/components/workout/Workout.tsx
+++ frontend/components/workout/Workout.tsx
@@ -1,18 +1,33 @@
 import { TWorkout } from "@/types/workout.type";
 import { EvilIcons } from "@expo/vector-icons";
 import moment from "moment";
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import { format, isYesterday } from "date-fns";
+import profileService from "@/lib/profileService";
 
 const Workout: FC<{ workout: TWorkout; navigation: any }> = ({
 	workout,
 	navigation,
 }) => {
+	const [username, setUsername] = React.useState<string>("");
 	const formatDuration = (durationSec: number) => {
 		const duration = moment.duration(durationSec, "seconds");
 		return `${duration.hours()}h ${duration.minutes()}m`;
 	};
+	
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				var profile = await profileService.getProfileById(workout.profileId);
+				setUsername(profile.username);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		fetchData();
+	}, []);
 
 	const publishDate: Date = new Date(workout.createdAt);
 	const createdAtDateString = isYesterday(publishDate)
@@ -23,14 +38,16 @@ const Workout: FC<{ workout: TWorkout; navigation: any }> = ({
 		<TouchableOpacity
 			onPress={() => navigation.navigate("Workout", { id: workout.id })}
 		>
-			<View className="mt-2 border-y-2">
-				<Text>Workout</Text>
-				<Text>{workout.name}</Text>
-				<Text>Volume: {workout.volumeKG}</Text>
-				<Text>Duration: {formatDuration(workout.durationSec)}</Text>
-				<Text>{createdAtDateString}</Text>
-				<Text>{workout.likedBy?.length ?? 0} likes</Text>
-				<EvilIcons name="like" size={24} />
+			<View className="mt-3">
+				<Text className="text-lg">{username}</Text>
+				<View className="border-y-2">
+					<Text>{workout.name}</Text>
+					<Text>Volume: {workout.volumeKG} kg</Text>
+					<Text>Duration: {formatDuration(workout.durationSec)}</Text>
+					<Text>{createdAtDateString}</Text>
+					<Text>{workout.likedBy?.length ?? 0} likes</Text>
+					<EvilIcons name="like" size={24} />
+				</View>
 			</View>
 		</TouchableOpacity>
 	);
