4ffbc87cfe39219da53331dcf9eeb5753504efe3
Michiel-Verhulst <michielverhulst@telenet.be>
2024-01-07 17:06:11 +0100

    new layout excercise info

--- frontend/app/exercise/index.tsx
+++ frontend/app/exercise/index.tsx
@@ -1,4 +1,5 @@
 import exerciseService from "@/lib/exerciseService";
+import { Ionicons } from "@expo/vector-icons";
 import { NavigationProp } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
 import { Dimensions, Text, TouchableOpacity, View, ScrollView, Image } from "react-native";
@@ -71,13 +72,35 @@ export default function ExerciseInfoPage({ route, navigation }: { route: any; na
 
   return (
     <ScrollView className="bg-white h-screen">
-      <View className="p-4">
+
+      <View className="px-4 pt-3">
+        <Text className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{exercise.name} <Text className="text-xl">({exercise.equipment})</Text></Text>
+        <Text>Primary: {exercise.type}</Text>
+        
+        <View className="mt-3 pb-3 border-b-2 border-gray-200">
+          <Text className="p-2 text-lg font-bold border-2 rounded-t-lg border-gray-500 bg-gray-800 text-white"><Ionicons name="barbell-outline" size={18} color={"white"}/> How To</Text>
+          <Text className="p-2 text-base border-x-2 border-b-2 rounded-b-lg border-gray-500 bg-gray-100">{exercise.description}</Text>
+        </View>
+      </View>
+
+      <View className="px-4 py-3">
+        <View className="border-b-2 border-gray-200 pb-3">
+        {/* Go to exercise history button */}
+        <TouchableOpacity
+          onPress={() => navigation.navigate("ExerciseHistory", { id: id, userid: userid, name: exercise.name })}
+          className="bg-gray-800 rounded py-2">
+          <Text className="text-center text-white font-bold text-lg">Go to Exercise History</Text>
+        </TouchableOpacity>
+        </View>
+      </View>
+
+      <View className="px-4 mb-10">
 
         {/* GRAPH VIEW */}
         {/* Verify if there is data available to display in the graph. */}
         {exerciseGraph && exerciseGraph.graph.length > 0 ? (
           <>
-            <Text className="font-bold text-xl">{changeToUserView[userSelected]}</Text>
+            <Text className="font-bold text-lg">{changeToUserView[userSelected]}</Text>
             <LineChart
               data={{
                 labels: exerciseGraph?.graph.map(
@@ -97,9 +120,9 @@ export default function ExerciseInfoPage({ route, navigation }: { route: any; na
               yAxisSuffix=""
               yAxisInterval={1} // optional, defaults to 1
               chartConfig={{
-                backgroundColor: "#3730a3",
-                backgroundGradientFrom: "#3730a3",
-                backgroundGradientTo: "#3730a3",
+                backgroundColor: "#1F2937",
+                backgroundGradientFrom: "#1F2937",
+                backgroundGradientTo: "#1F2937",
                 decimalPlaces: 0,
                 color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
                 labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
@@ -109,7 +132,7 @@ export default function ExerciseInfoPage({ route, navigation }: { route: any; na
                 propsForDots: {
                   r: "6",
                   strokeWidth: "2",
-                  stroke: "#3730a3",
+                  stroke: "#1F2937",
                 },
               }}
               bezier
@@ -123,35 +146,35 @@ export default function ExerciseInfoPage({ route, navigation }: { route: any; na
                 {/* Filter buttons */}
                 <TouchableOpacity
                   onPress={() => setUserSelected(userSelected === "volumeKG" ? "volumeKG" : "volumeKG")}
-                  className="bg-indigo-800 rounded mr-1 px-2 py-1"
+                  className="bg-gray-800 rounded mr-1 px-2 py-1"
                 >
                   <Text className="text-center text-white font-bold">Volume</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity
                   onPress={() => setUserSelected(userSelected === "one_rep_max" ? "volumeKG" : "one_rep_max")}
-                  className="bg-indigo-800 rounded mr-1 px-2 py-1"
+                  className="bg-gray-800 rounded mr-1 px-2 py-1"
                 >
                   <Text className="text-center text-white font-bold">One Rep Max</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity
                   onPress={() => setUserSelected(userSelected === "max_weight" ? "volumeKG" : "max_weight")}
-                  className="bg-indigo-800 rounded mr-1 px-2 py-1"
+                  className="bg-gray-800 rounded mr-1 px-2 py-1"
                 >
                   <Text className="text-center text-white font-bold">Max Weight</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity
                   onPress={() => setUserSelected(userSelected === "total_reps" ? "volumeKG" : "total_reps")}
-                  className="bg-indigo-800 rounded mr-1 px-2 py-1"
+                  className="bg-gray-800 rounded mr-1 px-2 py-1"
                 >
                   <Text className="text-center text-white font-bold">Total Reps</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity
                   onPress={() => setUserSelected(userSelected === "best_set_volume" ? "volumeKG" : "best_set_volume")}
-                  className="bg-indigo-800 rounded mr-1 px-2 py-1"
+                  className="bg-gray-800 rounded mr-1 px-2 py-1"
                 >
                   <Text className="text-center text-white font-bold">Best Set Volume</Text>
                 </TouchableOpacity>
@@ -165,17 +188,17 @@ export default function ExerciseInfoPage({ route, navigation }: { route: any; na
             <Text className="text-center font-bold text-lg">No graph data yet</Text>
           </View>
         )}
-
+            
         {/* BEST VIEW */}
         {/* Verify if there is data available to display in the records section. */}
         {exerciseBest && exerciseBest.personal_best[0].heaviest_weight !== null ? (
-          <View className="my-4 ">
-            <Text className="font-bold text-xl mt-4">Personal Records🥇</Text>
+          <View className="py-4">
+            <Text className="font-bold text-xl">Personal Records🥇</Text>
             <View>
-              <Text className="border-b-2 py-3 border-gray-300 font-bold">Heaviest Weight: {exerciseBest.personal_best[0].heaviest_weight} kg</Text>
-              <Text className="border-b-2 py-3 border-gray-300 font-bold">Best 1RM: {exerciseBest.personal_best[0].best_one_rep_max} kg</Text>
-              <Text className="border-b-2 py-3 border-gray-300 font-bold">Best Total Reps: {exerciseBest.personal_best[0].best_reps} kg</Text>
-              <Text className="border-b-2 py-3 border-gray-300 font-bold">
+              <Text className="border-b-2 py-3 border-gray-200 font-bold">Heaviest Weight: {exerciseBest.personal_best[0].heaviest_weight} kg</Text>
+              <Text className="border-b-2 py-3 border-gray-200 font-bold">Best 1RM: {exerciseBest.personal_best[0].best_one_rep_max} kg</Text>
+              <Text className="border-b-2 py-3 border-gray-200 font-bold">Best Total Reps: {exerciseBest.personal_best[0].best_reps} kg</Text>
+              <Text className="border-b-2 py-3 border-gray-200 font-bold">
                 Best Set Volume: {exerciseBest.personal_best[0].best_set_volume} kg ({exerciseBest.personal_best[0].set_volume_string})
               </Text>
             </View>
@@ -186,20 +209,6 @@ export default function ExerciseInfoPage({ route, navigation }: { route: any; na
             <Text className="text-center font-bold text-lg">No personal records yet</Text>
           </View>
         )}
-
-        {/* EXERCISE INFO VIEW */}
-        <View className="border-2 border-gray-300 mt-4">
-          <Text className="text-lg"><Text className="font-bold">Type:</Text> {exercise.type}</Text>
-          <Text className="text-lg mt-2"><Text className="font-bold">Equipment:</Text> {exercise.equipment}</Text>
-          <Text className="text-lg mt-2"><Text className="font-bold">How To:</Text> {exercise.description}</Text>
-        </View>
-
-        {/* Go to exercise history button */}
-        <TouchableOpacity
-          onPress={() => navigation.navigate("ExerciseHistory", { id: id, userid: userid, name: exercise.name })}
-          className="bg-gray-700 rounded mt-4 py-2">
-          <Text className="text-center text-white font-bold text-lg">Go to Exercise History</Text>
-        </TouchableOpacity>
 
       </View>
     </ScrollView>
